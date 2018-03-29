fc55cba757722e8c162273ea6563bff2558293f2
Mircea Lungu <mircea.lungu@gmail.com>
2018-03-29 14:44:45 +0200

    context maangers

---
+++ context_manager.py
@@ -0,0 +1,12 @@
+from contextlib import contextmanager
+
+
+@contextmanager
+def tag(name):
+    print(f'<{name}>')
+    yield
+    print(f'</{name}>')
+
+
+with tag('h1'):
+    print("important text")
